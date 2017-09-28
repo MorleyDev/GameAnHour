9f9440b1f6d09dd60ecbe9ce0143be3ffa8fb23d
Jason Morley <Jason.Morley@zibrant.com>
2017-09-28 13:52:34 +0100

    Input now uses fromEvent from rxjs

--- src/core/events/htmlelement-eventhandler.service.ts
+++ src/core/events/htmlelement-eventhandler.service.ts
@@ -1,29 +1,18 @@
+import { fromEvent } from "rxjs/observable/fromEvent";
 import { Observable } from "rxjs/Observable";
 import { Subject } from "rxjs/Subject";
 
 import { Key } from "../models/keys.model";
 import { EventHandler } from "./eventhandler.service";
 
-interface EventSource {
-	addEventListener(type: "keydown", listener: (event: KeyboardEvent) => any, useCapture?: boolean): void;
-	addEventListener(type: "keyup", listener: (event: KeyboardEvent) => any, useCapture?: boolean): void;
-}
-
 export class HtmlElementEventHandlerImpl implements EventHandler {
-	private readonly events = {
-		keyDown: new Subject<Key>(),
-		keyUp: new Subject<Key>(),
-	};
-
-	constructor(source: EventSource) {
-		source.addEventListener("keydown", ev => this.events.keyDown.next(ev.keyCode));
-		source.addEventListener("keyup", ev => this.events.keyUp.next(ev.keyCode));
+	constructor(private source: Document | Window | HTMLElement) {
 	}
 
 	public keyDown(): Observable<Key> {
-		return this.events.keyDown;
+		return fromEvent(this.source, "keydown");
 	}
 	public keyUp(): Observable<Key> {
-		return this.events.keyUp;
+		return fromEvent(this.source, "keyup");
 	}
 }
