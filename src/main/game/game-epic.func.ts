16094ed42c9e89f311f7de7638cbad715fb95798
Jason Morley <Jason.Morley@zibrant.com>
2017-10-03 18:04:50 +0100

    GameEpic now hard-references GameAction

--- src/main/game/game-epic.func.ts
+++ src/main/game/game-epic.func.ts
@@ -1,8 +1,10 @@
+import { Observable } from "rxjs/Observable";
 import { merge } from "rxjs/observable/merge";
 
 import { entityComponentEpic } from "../../entity-component/entity-component.epic";
+import { GameAction } from "./game-action.type";
 import { GameEpic } from "./game-epic.type";
 
 export const gameEpic: GameEpic = (action$, state) => merge(
-	entityComponentEpic(action$, state)
+	entityComponentEpic(action$, state) as Observable<GameAction>
 );
--- src/main/game/game-epic.type.ts
+++ src/main/game/game-epic.type.ts
@@ -4,4 +4,4 @@ import { Observable } from "rxjs/Observable";
 
 import { GameAction } from "./game-action.type";
 
-export type GameEpic = (action$: Observable<GameAction>, state: () => GameState) => Observable<GenericAction>;
+export type GameEpic = (action$: Observable<GameAction>, state: () => GameState) => Observable<GameAction>;
