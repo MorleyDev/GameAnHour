8a03e85d8a0c01895b023b7eadc5ee12afc5512c
Jason Morley <Jason.Morley@zibrant.com>
2017-11-03 18:01:25 +0000

    Added gitignore for cpp engine

--- engine/cpp/js/sfml.js
+++ engine/cpp/js/sfml.js
@@ -20,28 +20,11 @@
         }
     };
 
-    SFML_Render = function () {
-        return [
-            ["clear", { r: 255, g: 0, b: 0, a: 1 }],
-            [
-                [
-                    "origin",
-                    { x: 256, y: 256 },
-                    [
-                        ["rotate", 0, [
-                            ["fill", [
-                                { x: 10, y: 10 },
-                                { x: 100, y: 100 },
-                                { x: 10, y: 100 }
-                            ], { r: 0, g: 0, b: 255, a: 1 }]
-                        ]]
-                    ]
-                ],
-                ["stroke", [{ x: 100, y: 100 }, { x: 200, y: 150 }], { r: 0, g: 255, b: 0, a: 1 }],
-                ["stroke", { x: 300, y: 300, radius: 50 }, { r: 100, g: 200, b: 100, a: 1 }]
-            ]
-        ];
-    };
+    SFML_SetRenderer = function (renderer) {
+        SFML_Render = renderer;
+    }
+
+    SFML_Render = function () { return ["clear"]; };
 
     SFML_Events = {
         Closed: 0,                 ///< The window requested to be closed (no data)
